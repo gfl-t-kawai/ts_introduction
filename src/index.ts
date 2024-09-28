7a0ee9e6c742152f16f34cf95999e7591dad0ba5
t_kawai <t_kawai@global-future-link.co.jp>
2024-09-28 23:42:39 +0900

    非同期処理について

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,15 @@
 // tupleSample()
 
 // 07. ジェネリクス
-import genericsBasicSample from './generics/basic'
-import genericsAdvancedSample from './generics/advanced'
-genericsBasicSample()
-genericsAdvancedSample()
+// import genericsBasicSample from './generics/basic'
+// import genericsAdvancedSample from './generics/advanced'
+// genericsBasicSample()
+// genericsAdvancedSample()
+
+// 10. 非同期処理
+import callbackSample from './asynchronous/callback'
+import promiseSample from './asynchronous/promise'
+import asyncAwaitSample from './asynchronous/asyncAwait'
+// callbackSample()
+// promiseSample()
+asyncAwaitSample()
